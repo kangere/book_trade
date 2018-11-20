8bd7fc2b070f60c770302b0fee0c7a864b1d65aa
kenean dukamo <kad144@zips.uakorn.edu>
2018-11-19 19:06:12 -0500

    added search bar
    added search bar

--- include/utils.php
+++ include/utils.php
@@ -28,13 +28,29 @@
 		      <li>
 		        <a class=\"nav-link\" href=\"Store.php\">Book Store <span class=\"sr-only\">(current)</span></a>
 		      </li>
-		      <li class=\"nav-item\">
-		        <a class=\"nav-link\" href=\"home.php?q=logout\">Logout</a>
-		      </li>
 		    </ul>
+		    <form class=\"form-inline my-2 my-lg-0\">
+      			<input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">
+      			<button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">Search</button>
+    		</form>
+
+    	
+       
+      
+
 		  </div>
-		  <h4 class=\"my-2 my-lg-0\">".$fname." ".$lname."</h4>
-		</nav>";
+		  <li class=\"nav-item dropdown\">
+		        <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+		          ".$fname." ".$lname."
+		        </a>
+				  <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
+		          <a class=\"dropdown-item\" href=\"#\">Edit Profile</a>
+		          <a class=\"dropdown-item\" href=\"#\">Change Password</a>
+		          <div class=\"dropdown-divider\"></div>
+		          <a class=\"dropdown-item\" href=\"home.php?q=logout\">Logout</a>
+		        </div>
+        </li>
+		  </nav>";
 	}
 
 
